41f61ff752f0029adba5eaf844eda9eb8adde089
MikaelaMacari <55883910+MikaelaMacari@users.noreply.github.com>
2023-06-03 19:44:57 +0300

    Created project architecture.Added header view (#1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { ShoppingEditComponent } from './components/shopping-list-components/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './components/recipes-components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/recipes-components/recipe-list/recipe-item/recipe-item.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+ import {RecipeDetailComponent} from "./components/recipes-components/recipe-detail/recipe-detail.component";
+ import {ShoppingListComponent} from "./pages/shopping-list/shopping-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,
